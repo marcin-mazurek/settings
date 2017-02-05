1724421d84e9b7493872a58e7bbd0a969125ab6b
Marcin Mazurek <marcin@mazurek.pro>
2017-02-05 22:33:58 +0100

    Create install-apps.sh

---
+++ mac-setup/install-apps.sh
@@ -0,0 +1,9 @@
+# Install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install packages
+brew install node trash thefuck
+brew cask install google-chrome enpass alfred spectacle boom vlc jitouch
+
+# Install ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
