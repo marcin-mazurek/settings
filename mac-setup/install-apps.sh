a94ae222737d3af6500fe3dcbc3392ea0f2f752a
Marcin Mazurek <marcin@mazurek.pro>
2017-02-05 22:57:20 +0100

    Add communicators

--- mac-setup/install-apps.sh
+++ mac-setup/install-apps.sh
@@ -3,7 +3,7 @@
 
 # Install packages
 brew install node trash thefuck
-brew cask install google-chrome enpass alfred spectacle boom vlc jitouch iterm2
+brew cask install google-chrome enpass alfred spectacle boom vlc jitouch iterm2 messenger whatsapp
 
 # Install ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
