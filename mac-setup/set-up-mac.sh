eca0e027859819fbe42fd1b5ba155d940d932064
Marcin Mazurek <marcin@mazurek.pro>
2017-02-05 21:54:47 +0100

    Remove app install commands from set-up-mac.sh

--- mac-setup/set-up-mac.sh
+++ mac-setup/set-up-mac.sh
@@ -1,12 +1,3 @@
-# Install brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# Install brew packages
-brew install node trash
-
-# Install ZSH
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
 # Copy .zshrc
 cp ../zsh/.zshrc ~
 
