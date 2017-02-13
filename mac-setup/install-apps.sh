6ad235897a30a688862062aae4e231eec08bf4f0
Marcin Mazurek <marcin@mazurek.pro>
2017-02-13 21:56:31 +0100

    Add Finder Quick Look extensions

--- mac-setup/install-apps.sh
+++ mac-setup/install-apps.sh
@@ -2,8 +2,12 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install packages
+# System utils
 brew install node trash thefuck
+# Apps
 brew cask install google-chrome enpass alfred spectacle boom vlc jitouch iterm2 messenger whatsapp skype transmit flux the-unarchiver anki
+# Additional formats for Quick look
+brew cask install qlcolorcode qlmarkdown qlstephen quicklook-json betterzipql qlimagesize
 
 # Install ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
