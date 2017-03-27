26dbe948eb831054469a92e42e96a3b73e190a0f
Marcin Mazurek <marcin@mazurek.pro>
2017-03-27 09:25:48 +0200

    Create configure-apps.sh

---
+++ mac-setup/configure-apps.sh
@@ -0,0 +1,5 @@
+# Copy VS Code settings file
+cp ../vs-code/settings.json "$HOME/Library/Application Support/Code/User/settings.json"
+
+# Install VS Code extensions
+. ../vs-code/install-extensions.sh
