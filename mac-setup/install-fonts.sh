0218cb46cf12fa9b6e25b7a27a9b22fe410d1d14
Marcin Mazurek <marcin@mazurek.pro>
2018-09-24 16:40:30 +0200

    Create install-fonts.sh

---
+++ mac-setup/install-fonts.sh
@@ -0,0 +1,1 @@
+cd ~/Library/Fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
