198e8304c1b508f1eddc9f85c850c83a0fdd0da0
Marcin Mazurek <marcin@mazurek.pro>
2020-03-07 17:52:49 +0100

    Update install-apps.ps1

--- windows-setup/install-apps.ps1
+++ windows-setup/install-apps.ps1
@@ -1,1 +1,1 @@
-choco install googlechrome vscode enpass.install virtualbox notion hyper git microsoft-windows-terminal whatsapp slack doublecmd f.lux -y
+choco install googlechrome vscode enpass.install virtualbox notion hyper git microsoft-windows-terminal whatsapp slack doublecmd f.lux todoist -y
