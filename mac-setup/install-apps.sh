ac30b523e8b21d568330ad379d9848e01d8ed056
Marcin Mazurek <marcin@mazurek.pro>
2020-03-13 18:51:17 +0100

    Update install-apps.sh

--- mac-setup/install-apps.sh
+++ mac-setup/install-apps.sh
@@ -3,13 +3,13 @@
 
 # Install ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
 
 # Install packages
 ## System utils
-brew install hub node trash thefuck fzf zsh-syntax-highlighting zsh-autosuggestions diff-so-fancy tig autojump exa jq
+brew install hub nvm trash fzf zsh-syntax-highlighting zsh-autosuggestions diff-so-fancy tig autojump exa jq
 ## Docker
-brew cask install docker
+brew cask install docker enpass
 brew install bash-completion docker-completion docker-compose-completion docker-machine-completion
 ## Apps
 brew cask install visual-studio-code google-chrome enpass alfred spectacle vlc jitouch iterm2 messenger transmit flux the-unarchiver anki fbreader commander-one
