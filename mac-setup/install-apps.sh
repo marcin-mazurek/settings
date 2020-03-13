588cc84e7b63a1fd41811426ed4cbbb5bcacb845
Marcin Mazurek <marcin@mazurek.pro>
2020-03-13 22:05:15 +0100

    Update install-apps.sh

--- mac-setup/install-apps.sh
+++ mac-setup/install-apps.sh
@@ -9,10 +9,10 @@ git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
 ## System utils
 brew install hub nvm trash fzf zsh-syntax-highlighting zsh-autosuggestions diff-so-fancy tig autojump exa jq
 ## Docker
-brew cask install docker
+brew cask install docker google-cloud-cli
 brew install bash-completion docker-completion docker-compose-completion docker-machine-completion
 ## Apps
-brew cask install visual-studio-code google-chrome enpass alfred spectacle vlc jitouch iterm2 messenger transmit flux the-unarchiver commander-one
+brew cask install visual-studio-code google-chrome enpass alfred spectacle vlc jitouch iterm2 messenger transmit flux the-unarchiver notion commander-one
 ## Additional formats for Quick look
 brew cask install qlcolorcode qlmarkdown qlstephen quicklook-json betterzipql qlimagesize
 brew install Caskroom/cask/qlmobi
